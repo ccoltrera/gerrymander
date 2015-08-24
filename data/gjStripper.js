b3e5f94ed020303af34a1efaf4485ba78d1c91ad
Colin Coltrera <c.coltrera@gmail.com>
2015-08-24 15:36:51 -0700

    Add dependencies to package.json, modify gjStripper for use with current data

--- data/gjStripper.js
+++ data/gjStripper.js
@@ -2,20 +2,72 @@
 
 var jsonfile = require('jsonfile');
 
+var stateAbr = {
+  "Alabama": "AL",
+  "Alaska": "AK",
+  "Arizona": "AZ",
+  "Arkansas": "AR",
+  "California": "CA",
+  "Colorado": "CO",
+  "Connecticut": "CT",
+  "Delaware": "DE",
+  "District of Columbia": "DC",
+  "Florida": "FL",
+  "Georgia": "GA",
+  "Hawaii": "HI",
+  "Idaho": "ID",
+  "Illinois": "IL",
+  "Indiana": "IN",
+  "Iowa": "IA",
+  "Kansas": "KS",
+  "Kentucky": "KY",
+  "Louisiana": "LA",
+  "Maine": "ME",
+  "Maryland": "MD",
+  "Massachusetts": "MA",
+  "Michigan": "MI",
+  "Minnesota": "MN",
+  "Mississippi": "MS",
+  "Missouri": "MO",
+  "Montana": "MT",
+  "Nebraska": "NE",
+  "Nevada": "NV",
+  "New Hampshire": "NH",
+  "New Jersey": "NJ",
+  "New Mexico": "NM",
+  "New York": "NY",
+  "North Carolina": "NC",
+  "North Dakota": "ND",
+  "Ohio": "OH",
+  "Oklahoma": "OK",
+  "Oregon": "OR",
+  "Pennsylvania": "PA",
+  "Rhode Island": "RI",
+  "South Carolina": "SC",
+  "South Dakota": "SD",
+  "Tennessee": "TN",
+  "Texas": "TX",
+  "Utah": "UT",
+  "Vermont": "VT",
+  "Virginia": "VA",
+  "Washington": "WA",
+  "West Virginia": "WV",
+  "Wisconsin": "WI",
+  "Wyoming": "WY",
+};
+
 function gjStripper() {
-  jsonfile.readFile(process.argv[2], function(err, obj) {
-    for (var i = 0; i < obj.features.length; i++) {
-      delete obj.features[i].properties.description;
-      delete obj.features[i].properties.styleUrl;
-      delete obj.features[i].properties.styleHash;
-      delete obj.features[i].properties.id;
 
-
+  for (var state in stateAbr) {
+    var filename = './data/large/' + stateAbr[state] + '.geojson';
+    var geoobj = jsonfile.readFileSync(filename)
+    for (var i = 0; i < geoobj.features.length; i++) {
+      delete geoobj.features[i].properties.member;
     }
+    //console.log('./data/' + filename.substring(13,15) + '.less.geojson');
+    jsonfile.writeFileSync('./data/' + filename.substring(13,15) + '.less.geojson', geoobj)
+  }
 
-  jsonfile.writeFileSync(process.argv[2] + '.jsonfile', obj);
-
-  });
-}
+};
 
 gjStripper();
