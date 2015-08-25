63638e25ac84ccd5b71950d15ba84e5412c8ce03
Erik Sjaastad <spudlogic@gmail.com>
2015-08-25 13:36:41 -0700

    change FooterContainer to NavFrame

---
+++ src/NavFrame.jsx
@@ -0,0 +1,34 @@
+'use strict';
+var React = require('react');
+
+var NavFrame = React.createClass({
+  render: function() {
+    return (
+      <footer>
+        <ul id="mainMenue" role="navigation">
+          <li>
+            <a onclick={this.props.selectInfo(str)}><span className="text">PEOPE</span>
+            <span className="icon"></span></a>
+          </li>
+          <li>
+            <a onclick={this.openModal}><span className="text">JOBS</span>
+            <span className="icon"></span></a>
+          </li>
+          <li>
+            <a onclick={this.openModal}><span className="text">HOUSING</span>
+            <span className="icon"></span></a>
+          </li>
+          <li>
+            <a onclick={this.openModal}><span className="text">ECONOMIC</span>
+            <span className="icon"></span></a></li>
+          <li>
+            <a onclick={this.openModal}><span className="text">EDUCATION</span>
+            <span className="icon"></span></a>
+          </li>
+        </ul>
+      </footer>
+    );
+  }
+});
+
+module.exports = NavFrame;
