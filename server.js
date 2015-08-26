3e8a672acf86e294931ba3a23c5977171854d411
Colin Coltrera <c.coltrera@gmail.com>
2015-08-26 11:07:47 -0700

    Reset server.js and index.js to former states.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use(express.static('public/js'));
 
 app.listen(port, function() {
   console.log("Server running on port " + port);
