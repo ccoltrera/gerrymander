b6fc3ddff67775e90f6fd684694fab941284a58f
davschne <davschne@gmail.com>
2015-08-25 17:03:47 -0700

    Notes on how to populate info; index of full state name by FIPS

--- src/InfoDisplay.jsx
+++ src/InfoDisplay.jsx
@@ -1,17 +1,73 @@
 'use strict';
 var React = require('react');
+var censusData = require('./cen-rep-rep');
+
+var data = ['INFO', 'MONEY', 'EDUCATION', 'PEOPLE', 'ELECTIONS'];
+var statesByFIPS = require('../data/statesByFIPS');
+
+/*
+{
+  INFO: {
+    state: statesByFIPS[this.props.district.G.STATEFP],
+    districtNum: this.props.district.G.NAMELSAD,
+    currentRep: 'your mom',
+    whenDrawn: '2010 or so, I guess',
+    whoDrew: 'some jerkfaces'
+  },
+  MONEY: {
+    district: {
+      incomeByHousehold: getCDReport('hh_income'),
+      incomePerCapita: getCDReport('pc_income'),
+      incomeByHouseholdMedian: getCDReport('median_hh_income'),
+      publicAssistance: getCDReport('pub_assist')
+    },
+    state: {
+      incomeByHousehold: getStateReport('hh_income'),
+      incomePerCapita: getStateReport('pc_income'),
+      incomeByHouseholdMedian: getStateReport('median_hh_income'),
+      publicAssistance: getStateReport('pub_assist')
+    }
+  },
+  EDUCATION: {
+    district: {
+      attainments: getCDReport('ed_attain'),
+      languages: getCDReport('lang_spoken')
+    },
+    state: {
+      attainments: getStateReport('ed_attain'),
+      languages: getStateReport('lang_spoken')
+    }
+  },
+  PEOPLE: {
+    district: {
+      populationTotal: getCDReport('total_pop'),
+      populationDensity: {total/area},
+      ethnicity: getCDReport('race')
+    },
+    state: {
+      populationTotal: getCDReport('total_pop'),
+      populationDensity: {total/area},
+      ethnicity: getCDReport('race')
+    }
+  },
+  ELECTIONS: {
+    2012 votes
+  }
+}
+
+*/
 
 var InfoDisplay = React.createClass({
+  getInitialState: function() {
+    return {
+      data: null
+    }
+  },
   render: function() {
+    console.log(this.props.district);
     return (
       <aside className="right-side-info">
-        <p>This pannel shows up when a district is clicked</p>
-        <p>Add close button</p>
-        <button>PEOPLE</button><br />
-        <button>JOBS</button><br />
-        <button>HOUSING</button><br />
-        <button>ECONOMIC</button><br />
-        <button>EDUCATION</button><br />
+        <p></p>
       </aside>
     );
   }
