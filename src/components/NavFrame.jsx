9a980b0601d05926414ae0485ee0dabb37f7f466
Erik Sjaastad <spudlogic@gmail.com>
2015-08-27 11:15:37 -0700

    add active the React way...

--- src/components/NavFrame.jsx
+++ src/components/NavFrame.jsx
@@ -1,26 +1,10 @@
 'use strict';
 var React = require('react');
-function addClass(id,new_class){
-  var i,n=0;
-  var navEls = document.getElementsByTagName('li');
-  console.log(navEls);
-  //var cur = navEls.querySelector('li.active');
-  //if (cur) cur.classList.remove('active');
-  new_class=new_class.split(",");
-  for(i=0;i<new_class.length;i++){
-    if((" "+document.getElementById(id).className+" ").indexOf(" "+new_class[i]+" ")==-1){
-      document.getElementById(id).className+=new_class[i];
-      n++;
-    }
-  }
-  return n;
-}
+
 var NavFrame = React.createClass({
   buttons: ['INFO', 'MONEY', 'EDUCATION', 'PEOPLE', 'ELECTIONS'],
   clickHandle: function(i) {
     this.props.selectInfoType(this.buttons[i]);
-    addClass('info-frame','active');
-    addClass(this.buttons[i], 'active');
   },
 
   render: function() {
